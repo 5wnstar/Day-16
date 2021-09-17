220973601e6f5d3615e96b9b6c4e1fee0369415a
Cyber-Jollof <53369822+Cyb3r-J0ll0f@users.noreply.github.com>
2021-09-17 13:25:57 +0100

    Create DataTypes.py

---
+++ DataTypes.py
@@ -0,0 +1,26 @@
+#Here are some examples of number data type in Python.
+
+i = 10
+f = 23.43
+total = 987
+print(total)
+
+#Here is an example of string data type in Python.
+
+str = "Hello World, I am Python Data Types"
+print(str)
+
+#Here is an example of list data type in Python.
+
+list = [10, 20, 30, 'hi', 'hello', 43.23, 432.42, 'python']
+print(list)
+
+#Here is an example of tuple data type in Python.
+
+tuple = (10, 20, 30, 'hi', 'hello', 43.23, 432.42, 'python')
+print(tuple)
+
+#Here is an example of dictionary data type in Python.
+
+dict = {'number': 10, 'code': 20, 'name': 'python'}
+print(dict)
